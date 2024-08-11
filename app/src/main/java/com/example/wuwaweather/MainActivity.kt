5c4bc6558935920c0aa53dd9bdce6ecb208a0f52
FhamidAurnob <fmaurnob0@gmail.com>
2024-08-11 17:39:50 +0600

    updated
    updated

---
+++ app/src/main/java/com/example/wuwaweather/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.wuwaweather
+
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val source : ImageDecoder.Source = ImageDecoder.createSource(
+            resources, R.drawable.sunnylong)
+
+        val drawable : Drawable = ImageDecoder.decodeDrawable(source)
+
+        val imageView : ImageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageDrawable(drawable)
+
+        (drawable as? AnimatedImageDrawable)?.start()
+        }
+    }
